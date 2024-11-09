07bb1de8a35a463efc32eb9918a94216a6fa4497
Adam Abouljoud <62528589+adamabljd@users.noreply.github.com>
2024-11-09 02:43:06 +0100

    fixed web problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Game from './pages/Game';
 import MainMenuPage from './pages/MainMenuPage';
 import BestTimesPage from './pages/BestTimesPage';
@@ -54,7 +54,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/">
       <div className="min-h-screen bg-beige px-3">
         <Routes>
           <Route path="/" element={<MainMenuPage
